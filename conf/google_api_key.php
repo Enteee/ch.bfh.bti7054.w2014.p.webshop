e33fff39d3329d8a14becb80dbfeb8c30e1cc514
Ente <ducksource@duckpond.ch>
2015-01-08 02:09:16 +0100

    for easy install: add google api key

---
+++ conf/google_api_key.php
@@ -0,0 +1,5 @@
+<?php
+$config['google_auth']['client_id']     = '709088140292-nq0r7u21mngevs9agtgbsrraq3mmenlp.apps.googleusercontent.com';
+$config['google_auth']['client_secret'] = 'cutPrN6qqd55GN7W91oueBXF';
+$config['google_auth']['redirect_uri']  = 'https://localhost/?action\u003doauth2callback';
+?>
